7c31e4c3da4ee8efba5223760e8cdb311501da90
kaushalsingh584 <64363016+kaushalsingh584@users.noreply.github.com>
2023-02-21 23:18:22 +0530

    Update settings.py

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -146,4 +146,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'thakursahabji993@gmail.com'
-EMAIL_HOST_PASSWORD = 'hcoluprxzskachml'
+EMAIL_HOST_PASSWORD = 'your-password'
